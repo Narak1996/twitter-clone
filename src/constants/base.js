a3911ab52ccd285c41ba6efffca6202ad2886479
Chann <netsolutiononlinegroup@gmail.com>
2024-03-30 11:38:54 +0700

    fix

--- src/constants/base.js
+++ src/constants/base.js
@@ -1,5 +1,5 @@
-// const base_url = 'https://twitterak-a5hhd.ondigitalocean.app/api'
-const base_url = 'http://localhost:3000/api'
+const base_url = 'https://twitterak-a5hhd.ondigitalocean.app/api'
+// const base_url = 'http://localhost:3000/api'
 
 
 module.exports={base_url}
