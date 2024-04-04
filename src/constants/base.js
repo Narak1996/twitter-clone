8b2ba527af1bbc8d7de17da5d953acc89fadbcc1
Chann <netsolutiononlinegroup@gmail.com>
2024-04-04 12:23:02 +0700

    - update base url

--- src/constants/base.js
+++ src/constants/base.js
@@ -1,3 +1,4 @@
-const base_url = 'https://twitterak-a5hhd.ondigitalocean.app/api'
-// const base_url = 'http://localhost:3000/api'
+
+// const base_url = 'https://twitterak-a5hhd.ondigitalocean.app/api'
+const base_url = process.env.VUE_APP_ENV_BASE_URL
 module.exports={base_url}
